7ac87d6e2a9a5e50aa7869189fe48ff167f418dc
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2019-11-07 23:05:49 +0100

    Use DSL to simplify responses
    Signed-off-by: Dragutin Marjanovic <dmarjanovic94@gmail.com>

--- modules/example/src/main/scala/zio/telemetry/example/http/StatusesService.scala
+++ modules/example/src/main/scala/zio/telemetry/example/http/StatusesService.scala
@@ -4,8 +4,8 @@ import io.circe.Encoder
 import io.opentracing.propagation.Format.Builtin.{ HTTP_HEADERS => HttpHeadersFormat }
 import io.opentracing.propagation.TextMapAdapter
 import org.http4s.circe.jsonEncoderOf
-import org.http4s.dsl.io._
-import org.http4s.{ EntityEncoder, HttpRoutes, Response }
+import org.http4s.dsl.Http4sDsl
+import org.http4s.{ EntityEncoder, HttpRoutes }
 import sttp.model.Uri
 import zio.ZManaged
 import zio.clock.Clock
@@ -18,6 +18,8 @@ import scala.jdk.CollectionConverters._
 object StatusesService {
 
   def statuses(backendUri: Uri, service: ZManaged[Clock, Throwable, Clock with OpenTracing]): HttpRoutes[AppTask] = {
+    val dsl: Http4sDsl[AppTask] = Http4sDsl[AppTask]
+    import dsl._
 
     implicit def encoder[A: Encoder]: EntityEncoder[AppTask, A] = jsonEncoderOf[AppTask, A]
 
@@ -31,9 +33,9 @@ object StatusesService {
             res <- Client
                     .status(backendUri)
                     .map(_.body)
-                    .map {
-                      case Right(s) => Response(Ok).withEntity(Statuses(List(s, StatusUp)))
-                      case _        => Response(Ok).withEntity(Statuses(List(Status("backend", "1.0.0", "down"), StatusUp)))
+                    .flatMap {
+                      case Right(s) => Ok(Statuses(List(s, StatusUp)))
+                      case _        => Ok(Statuses(List(Status("backend", "1.0.0", "down"), StatusUp)))
                     }
           } yield res).provide(env)
         }
