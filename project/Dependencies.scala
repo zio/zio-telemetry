f819d168689bfd1b4318769800ea63a8560889b0
Scala Steward <me@scala-steward.org>
2020-05-04 23:25:23 +0200

    Update opentelemetry-api, ... to 0.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val jaeger        = "1.2.0"
     val sttp          = "2.1.0"
     val opentracing   = "0.33.0"
-    val opentelemetry = "0.3.0"
+    val opentelemetry = "0.4.0"
     val zipkin        = "2.12.3"
     val zio           = "1.0.0-RC18-2"
   }
