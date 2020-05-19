11b517e7bd80187c3573f246ec20471f475ea5f0
Scala Steward <me@scala-steward.org>
2020-05-19 10:57:19 +0200

    Update zio, zio-test, zio-test-sbt to 1.0.0-RC19-1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val opentracing   = "0.33.0"
     val opentelemetry = "0.3.0"
     val zipkin        = "2.15.0"
-    val zio           = "1.0.0-RC19"
+    val zio           = "1.0.0-RC19-1"
   }
 
   lazy val zio = Seq(
