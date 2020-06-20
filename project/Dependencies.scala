eb29494c28ab267b60d7fddaadba2e511d29ff5e
Scala Steward <me@scala-steward.org>
2020-06-20 02:37:23 +0200

    Update zio, zio-test, zio-test-sbt to 1.0.0-RC21

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val opentelemetry = "0.3.0"
     val opencensus    = "0.26.0"
     val zipkin        = "2.15.0"
-    val zio           = "1.0.0-RC20"
+    val zio           = "1.0.0-RC21"
   }
 
   lazy val zio = Seq(
