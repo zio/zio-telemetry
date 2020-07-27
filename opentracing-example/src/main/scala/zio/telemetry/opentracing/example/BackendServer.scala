575cf74a696f9a1a4e59d9f6eec068924816530d
Simon Schenk <simon@schenk-online.net>
2020-07-27 15:36:19 +0200

    Update http4s to 0.21.6 and cats interop to 2.1.4.0RC17

--- opentracing-example/src/main/scala/zio/telemetry/opentracing/example/BackendServer.scala
+++ opentracing-example/src/main/scala/zio/telemetry/opentracing/example/BackendServer.scala
@@ -4,25 +4,32 @@ import cats.effect.{ ExitCode => catsExitCode }
 import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.syntax.kleisli._
+import zio.{ ExitCode, ZEnv, ZIO }
+import zio.clock.Clock
 import zio.interop.catz._
 import zio.telemetry.opentracing.example.JaegerTracer.makeService
 import zio.telemetry.opentracing.example.config.Configuration
 import zio.telemetry.opentracing.example.http.{ AppTask, StatusService }
-import zio.{ ExitCode, ZEnv, ZIO }
 
 object BackendServer extends CatsApp {
 
-  override def run(args: List[String]): ZIO[ZEnv, Nothing, ExitCode] =
-    (for {
-      conf    <- Configuration.load.provideLayer(Configuration.live)
-      service = makeService(conf.tracer.host, "zio-backend")
-      router  = Router[AppTask]("/" -> StatusService.status(service)).orNotFound
-      result <- BlazeServerBuilder[AppTask]
-                 .bindHttp(conf.backend.port, conf.backend.host)
-                 .withHttpApp(router)
-                 .serve
-                 .compile[AppTask, AppTask, catsExitCode]
-                 .drain
-                 .as(ExitCode.success)
-    } yield result).orElse(ZIO.succeed(ExitCode.failure))
+  override def run(args: List[String]): ZIO[ZEnv, Nothing, ExitCode] = {
+    val exit =
+      ZIO.runtime[Clock].flatMap { implicit runtime =>
+        implicit val ec = runtime.platform.executor.asEC
+        for {
+          conf    <- Configuration.load.provideLayer(Configuration.live)
+          service = makeService(conf.tracer.host, "zio-backend")
+          router  = Router[AppTask]("/" -> StatusService.status(service)).orNotFound
+          result <- BlazeServerBuilder[AppTask](ec)
+                     .bindHttp(conf.backend.port, conf.backend.host)
+                     .withHttpApp(router)
+                     .serve
+                     .compile[AppTask, AppTask, catsExitCode]
+                     .drain
+                     .as(ExitCode.success)
+        } yield result
+      }
+    exit.orElse(ZIO.succeed(ExitCode.failure))
+  }
 }
