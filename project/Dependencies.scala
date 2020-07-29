392ea7ce652dcbdbe9c92b073159631b3992ad79
Scala Steward <me@scala-steward.org>
2020-07-30 01:00:41 +0200

    Update grpc-netty-shaded to 1.31.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -61,6 +61,6 @@ object Dependencies {
   lazy val opentelemetryExample = example ++ Seq(
     "io.opentelemetry" % "opentelemetry-exporters-jaeger" % Versions.opentelemetry,
     "io.opentelemetry" % "opentelemetry-sdk"              % Versions.opentelemetry,
-    "io.grpc"          % "grpc-netty-shaded"              % "1.30.2"
+    "io.grpc"          % "grpc-netty-shaded"              % "1.31.0"
   )
 }
