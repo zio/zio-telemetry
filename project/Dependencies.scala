d8977d82136294be3fe4cc3bd3f18f8d333f6e68
Scala Steward <me@scala-steward.org>
2020-10-03 23:17:11 +0200

    Update zio-interop-cats to 2.1.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val opencensus     = "0.26.0"
     val zipkin         = "2.15.2"
     val zio            = "1.0.1"
-    val zioInteropCats = "2.1.4.0"
+    val zioInteropCats = "2.1.4.1"
   }
 
   lazy val zio = Seq(
