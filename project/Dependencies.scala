50ad075c5b0d84b4ae85f71a3be2aff81672c313
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-11-09 06:54:11 +0100

    Update zio, zio-test, zio-test-sbt to 1.0.3 (#240)
    Co-authored-by: Simon Schenk <simon@schenk-online.net>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val opentelemetry  = "0.7.1"
     val opencensus     = "0.26.0"
     val zipkin         = "2.15.2"
-    val zio            = "1.0.1"
+    val zio            = "1.0.3"
     val zioInteropCats = "2.1.4.0"
   }
 
