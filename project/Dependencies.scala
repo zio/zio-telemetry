a852549550605dce2fac6bea6a79ddeb62859efc
Scala Steward <me@scala-steward.org>
2020-11-11 21:54:43 +0100

    Update http4s-blaze-server, http4s-circe, ... to 0.21.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val http4s         = "0.21.8"
+    val http4s         = "0.21.9"
     val jaeger         = "1.4.0"
     val sttp           = "2.2.9"
     val opentracing    = "0.33.0"
