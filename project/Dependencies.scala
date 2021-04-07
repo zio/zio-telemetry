ffd067978cf3b3a323be0c1255fdb8c82c6d1b60
Scala Steward <me@scala-steward.org>
2021-04-07 21:20:41 +0200

    Update jaeger-client, jaeger-core, ... to 1.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val http4s         = "0.21.21"
-    val jaeger         = "1.5.0"
+    val jaeger         = "1.6.0"
     val sttp           = "2.2.9"
     val opentracing    = "0.33.0"
     val opentelemetry  = "1.1.0"
