65b00875ff950acb0cbfe7ae11f86a6b910ac679
Scala Steward <me@scala-steward.org>
2021-05-28 16:14:09 +0200

    Update scala-library to 2.12.14

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -12,7 +12,7 @@ object BuildHelper {
     incOptions ~= (_.withLogRecompileOnMacro(false))
   )
 
-  private val Scala212 = "2.12.13"
+  private val Scala212 = "2.12.14"
   private val Scala213 = "2.13.5"
 
   private val stdOptions = Seq(
