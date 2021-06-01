09f70b44f50714ccd5ddba29e54e8bbc7a65fad0
Dejan Mijic <dmijic@acm.org>
2021-06-01 11:48:04 +0200

    Resolve versions from ci setup

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -12,8 +12,21 @@ object BuildHelper {
     incOptions ~= (_.withLogRecompileOnMacro(false))
   )
 
-  private val Scala212 = "2.12.14"
-  private val Scala213 = "2.13.6"
+  private val versions: Map[String, String] = {
+    import org.snakeyaml.engine.v2.api.{Load, LoadSettings}
+
+    import java.util.{List => JList, Map => JMap}
+    import scala.jdk.CollectionConverters._
+
+    val doc  = new Load(LoadSettings.builder().build())
+      .loadFromReader(scala.io.Source.fromFile(".github/workflows/ci.yml").bufferedReader())
+    val yaml = doc.asInstanceOf[JMap[String, JMap[String, JMap[String, JMap[String, JMap[String, JList[String]]]]]]]
+    val list = yaml.get("jobs").get("test").get("strategy").get("matrix").get("scala").asScala
+    list.map(v => (v.split('.').take(2).mkString("."), v)).toMap
+  }
+
+  private val Scala212: String = versions("2.12")
+  private val Scala213: String = versions("2.13")
 
   private val stdOptions = Seq(
     "-deprecation",
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,3 +4,6 @@ addSbtPlugin("ch.epfl.scala"                     % "sbt-bloop"        % "1.4.8")
 addSbtPlugin("io.github.davidgregory084"         % "sbt-tpolecat"     % "0.1.18")
 addSbtPlugin("org.scalameta"                     % "sbt-scalafmt"     % "2.4.2")
 addSbtPlugin("org.scoverage"                     % "sbt-scoverage"    % "1.8.2")
+
+libraryDependencies += "org.snakeyaml" % "snakeyaml-engine" % "2.3"
+
