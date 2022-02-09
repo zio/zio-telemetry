b0cf3de257b5f03097e16eb689cd76bb949cdbd3
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-02-09 14:47:19 +0100

    Update opencensus-api, ... to 0.31.0 (#491)
    Co-authored-by: Dejan Mijić <dejan.mjc@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val sttp3         = "3.3.14"
     val opentracing   = "0.33.0"
     val opentelemetry = "1.11.0"
-    val opencensus    = "0.30.0"
+    val opencensus    = "0.31.0"
     val zipkin        = "2.16.3"
     val zio           = "1.0.13"
     val zioHttp       = "1.0.0.0-RC17"
