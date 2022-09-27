86a4258ff8dd5a55de98c1d1a76dba4e55960b35
Michael Nedokushev <grouzen.hexy@gmail.com>
2022-09-28 01:37:27 +0300

    Fix warning

--- opentracing-example/src/main/scala/zio/telemetry/opentracing/example/http/ProxyHttpApp.scala
+++ opentracing-example/src/main/scala/zio/telemetry/opentracing/example/http/ProxyHttpApp.scala
@@ -4,7 +4,6 @@ import io.opentracing.propagation.Format.Builtin.{ HTTP_HEADERS => HttpHeadersFo
 import io.opentracing.propagation.TextMapAdapter
 import io.opentracing.tag.Tags
 import sttp.model.Method.GET
-import sttp.model.Uri
 import zhttp.http.{ !!, ->, /, Http, HttpApp, Method, Response }
 import zio.json.EncoderOps
 import zio.telemetry.opentracing.OpenTracing
