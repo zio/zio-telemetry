1474befa04ac922f6b4dfdfcf1048cd8e068879d
Scala Steward <scala_steward@virtuslab.com>
2022-11-17 06:21:40 +0000

    Update opentelemetry-api, ... to 1.20.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val opentracing           = "0.33.0"
-    val opentelemetry         = "1.18.0"
+    val opentelemetry         = "1.20.1"
     val opencensus            = "0.31.1"
     val scalaCollectionCompat = "2.8.1"
     val zio                   = "2.0.2"
