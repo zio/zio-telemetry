f179976ac761e170a7d6632e13261c5ceb6bb070
IvanFinochenko <ifinochenko@gmail.com>
2023-05-16 15:45:21 +0300

    add tests opencensus (#683)

---
+++ opencensus/src/test/scala/zio/telemetry/opencensus/InMemoryExporter.scala
@@ -0,0 +1,30 @@
+package zio.telemetry.opencensus
+
+import io.opencensus.trace.`export`.SpanData
+import io.opencensus.trace.`export`.SpanExporter.Handler
+import io.opencensus.trace.{ Tracing => OTracing }
+import zio.{ Ref, Runtime, Unsafe, ZIO }
+
+import java.util
+import scala.jdk.CollectionConverters._
+
+object InMemoryExporter {
+
+  private val runtime = Runtime.default
+
+  def register(): ZIO[Any, Nothing, Ref[List[SpanData]]] =
+    Ref
+      .make(List.empty[SpanData])
+      .map { x =>
+        OTracing.getExportComponent.getSpanExporter.registerHandler("InMemoryExporter", new InMemoryExporter(x))
+        x
+      }
+
+  class InMemoryExporter(finishedSpans: Ref[List[SpanData]]) extends Handler {
+    override def `export`(spanDataList: util.Collection[SpanData]): Unit =
+      Unsafe.unsafe { implicit unsafe =>
+        runtime.unsafe.run(finishedSpans.update(x => x ++ spanDataList.asScala.toList))
+        ()
+      }
+  }
+}
