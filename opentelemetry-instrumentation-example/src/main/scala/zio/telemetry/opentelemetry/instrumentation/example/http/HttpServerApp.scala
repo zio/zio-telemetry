5bb78fe1d6fd3a66d5818cea7c329414812ad6b3
Michael Nedokushev <grouzen.hexy@gmail.com>
2023-05-20 18:10:42 +0100

    Fix internal health span

--- opentelemetry-instrumentation-example/src/main/scala/zio/telemetry/opentelemetry/instrumentation/example/http/HttpServerApp.scala
+++ opentelemetry-instrumentation-example/src/main/scala/zio/telemetry/opentelemetry/instrumentation/example/http/HttpServerApp.scala
@@ -1,33 +1,16 @@
 package zio.telemetry.opentelemetry.instrumentation.example.http
 
-import io.opentelemetry.api.trace.SpanKind
 import zhttp.http._
 import zio._
-import zio.telemetry.opentelemetry.context.IncomingContextCarrier
 import zio.telemetry.opentelemetry.tracing.Tracing
-import zio.telemetry.opentelemetry.tracing.propagation.TraceContextPropagator
 
 case class HttpServerApp(tracing: Tracing) {
 
   import tracing.aspects._
 
-  def headersCarrier(initial: Headers): IncomingContextCarrier[Headers] =
-    new IncomingContextCarrier[Headers] {
-      override val kernel: Headers = initial
-
-      override def getAllKeys(carrier: Headers): Iterable[String] =
-        carrier.headers.headersAsList.map(_._1)
-
-      override def getByKey(carrier: Headers, key: String): Option[String] =
-        carrier.headers.headerValue(key)
-
-    }
-
   val routes: HttpApp[Any, Throwable] =
-    Http.collectZIO { case request @ Method.GET -> _ / "health" =>
-      val carrier = headersCarrier(request.headers)
-
-      health @@ extractSpan(TraceContextPropagator.default, carrier, "health-logic-span", SpanKind.INTERNAL)
+    Http.collectZIO { case _ @Method.GET -> _ / "health" =>
+      health @@ span("health-endpoint")
     }
 
   def health: UIO[Response] =
