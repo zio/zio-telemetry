bbb4dfe3b1193f38289366f094ad88f0f82daa0a
Scala Steward <scala_steward@virtuslab.com>
2023-06-17 01:07:35 +0000

    Update scala-collection-compat to 2.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val opentracing           = "0.33.0"
     val opentelemetry         = "1.27.0"
     val opencensus            = "0.31.1"
-    val scalaCollectionCompat = "2.10.0"
+    val scalaCollectionCompat = "2.11.0"
     val zio                   = "2.0.15"
   }
 
