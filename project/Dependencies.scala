81e0e3dc39924dab19ce99f39d743ff76c06fa1b
Michael Nedokushev <grouzen.hexy@gmail.com>
2023-12-28 14:06:39 +0000

    Update zio to 2.0.20 (#792)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val opentelemetry         = "1.33.0"
     val opencensus            = "0.31.1"
     val scalaCollectionCompat = "2.11.0"
-    val zio                   = "2.0.19"
+    val zio                   = "2.0.20"
   }
 
   object Orgs {
