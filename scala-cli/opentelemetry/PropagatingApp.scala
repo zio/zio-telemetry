16a16a6541a3a658af13d950d2dd50654e876745
Scala Steward <scala_steward@virtuslab.com>
2024-06-08 22:06:48 +0000

    Update opentelemetry-api, ... to 1.39.0

--- scala-cli/opentelemetry/PropagatingApp.scala
+++ scala-cli/opentelemetry/PropagatingApp.scala
@@ -1,9 +1,9 @@
 //> using scala "2.13.14"
 //> using dep dev.zio::zio:2.1.2
 //> using dep dev.zio::zio-opentelemetry:3.0.0-RC24
-//> using dep io.opentelemetry:opentelemetry-sdk:1.38.0
-//> using dep io.opentelemetry:opentelemetry-sdk-trace:1.38.0
-//> using dep io.opentelemetry:opentelemetry-exporter-logging-otlp:1.38.0
+//> using dep io.opentelemetry:opentelemetry-sdk:1.39.0
+//> using dep io.opentelemetry:opentelemetry-sdk-trace:1.39.0
+//> using dep io.opentelemetry:opentelemetry-exporter-logging-otlp:1.39.0
 //> using dep io.opentelemetry.semconv:opentelemetry-semconv:1.22.0-alpha
 
 import io.opentelemetry.exporter.logging.otlp.OtlpJsonLoggingSpanExporter
