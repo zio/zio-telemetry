3174745262686688929fd944c861a04395321083
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-07-31 17:37:49 +0200

    Update zio, zio-stacktracer, zio-test, ... to 2.1.6 (#859)
    Co-authored-by: Michael Nedokushev <grouzen.hexy@gmail.com>

--- scala-cli/opentelemetry-zio-logging/ZioLoggingApp.scala
+++ scala-cli/opentelemetry-zio-logging/ZioLoggingApp.scala
@@ -1,5 +1,5 @@
 //> using scala "2.13.14"
-//> using dep dev.zio::zio:2.1.5
+//> using dep dev.zio::zio:2.1.6
 //> using dep dev.zio::zio-opentelemetry:3.0.0-RC24
 //> using dep dev.zio::zio-opentelemetry-zio-logging:3.0.0-RC24
 //> using dep io.opentelemetry:opentelemetry-sdk:1.40.0
