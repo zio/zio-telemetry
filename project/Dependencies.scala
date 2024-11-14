6c7ebded03c8488f2541f22cf812d8db65c7a35e
Scala Steward <scala_steward@virtuslab.com>
2024-11-14 01:04:32 +0000

    Update zio-logging to 2.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val opencensus            = "0.31.1"
     val scalaCollectionCompat = "2.12.0"
     val zio                   = "2.1.12"
-    val zioLogging            = "2.3.2"
+    val zioLogging            = "2.4.0"
     val izumiReflect          = "2.3.10"
   }
 
