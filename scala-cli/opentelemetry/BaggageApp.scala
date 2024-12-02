68b8b718a013f8a948f761617d41d8d81d0a7858
Scala Steward <scala_steward@virtuslab.com>
2024-12-02 08:13:36 +0000

    Update zio-opentelemetry, ... to 3.1.0

--- scala-cli/opentelemetry/BaggageApp.scala
+++ scala-cli/opentelemetry/BaggageApp.scala
@@ -1,6 +1,6 @@
 //> using scala "3.5.2"
 //> using dep dev.zio::zio:2.1.13
-//> using dep dev.zio::zio-opentelemetry:3.0.2
+//> using dep dev.zio::zio-opentelemetry:3.1.0
 
 import zio.*
 import zio.telemetry.opentelemetry.baggage.Baggage
