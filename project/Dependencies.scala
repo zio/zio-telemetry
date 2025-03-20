96ab066e9e6d3ee9417f495847e53090d10e0a16
Scala Steward <scala_steward@virtuslab.com>
2025-03-20 08:37:23 +0000

    Update izumi-reflect to 3.0.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val scalaCollectionCompat = "2.13.0"
     val zio                   = "2.1.16"
     val zioLogging            = "2.5.0"
-    val izumiReflect          = "3.0.1"
+    val izumiReflect          = "3.0.2"
   }
 
   object Orgs {
