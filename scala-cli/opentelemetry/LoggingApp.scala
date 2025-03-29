2e6648eeed22427fb84c969c19c5f0f9a033465f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-03-29 23:37:37 +0100

    Update scala3-library to 3.6.4 (#959)

--- scala-cli/opentelemetry/LoggingApp.scala
+++ scala-cli/opentelemetry/LoggingApp.scala
@@ -1,4 +1,4 @@
-//> using scala "3.6.3"
+//> using scala "3.6.4"
 //> using dep dev.zio::zio:2.1.16
 //> using dep dev.zio::zio-opentelemetry:3.1.2
 //> using dep io.opentelemetry:opentelemetry-sdk:1.48.0
