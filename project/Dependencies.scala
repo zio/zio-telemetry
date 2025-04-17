766adc10ed16aa67a06e81dbfef8ce4c3d33dd3a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-04-17 14:40:23 +0200

    Update opentelemetry-api, ... to 1.49.0 (#969)
    Co-authored-by: Michael Nedokushev <grouzen.hexy@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val opentracing           = "0.33.0"
-    val opentelemetry         = "1.48.0"
+    val opentelemetry         = "1.49.0"
     val opencensus            = "0.31.1"
     val scalaCollectionCompat = "2.13.0"
     val zio                   = "2.1.16"
