60e727f99553cfe6e9035acf97d1c3bccd40f334
Scala Steward <scala_steward@virtuslab.com>
2025-05-11 05:02:14 +0000

    Update opentelemetry-api, ... to 1.50.0

--- scala-cli/opentelemetry-zio-logging/ZioLoggingApp.scala
+++ scala-cli/opentelemetry-zio-logging/ZioLoggingApp.scala
@@ -2,9 +2,9 @@
 //> using dep dev.zio::zio:2.1.17
 //> using dep dev.zio::zio-opentelemetry:3.1.4
 //> using dep dev.zio::zio-opentelemetry-zio-logging:3.1.4
-//> using dep io.opentelemetry:opentelemetry-sdk:1.49.0
-//> using dep io.opentelemetry:opentelemetry-sdk-trace:1.49.0
-//> using dep io.opentelemetry:opentelemetry-exporter-logging-otlp:1.49.0
+//> using dep io.opentelemetry:opentelemetry-sdk:1.50.0
+//> using dep io.opentelemetry:opentelemetry-sdk-trace:1.50.0
+//> using dep io.opentelemetry:opentelemetry-exporter-logging-otlp:1.50.0
 //> using dep io.opentelemetry.semconv:opentelemetry-semconv:1.32.0
 
 import io.opentelemetry.exporter.logging.otlp.OtlpJsonLoggingSpanExporter
--- scala-cli/opentelemetry/MetricsApp.scala
+++ scala-cli/opentelemetry/MetricsApp.scala
@@ -1,9 +1,9 @@
 //> using scala "3.6.4"
 //> using dep dev.zio::zio:2.1.17
 //> using dep dev.zio::zio-opentelemetry:3.1.4
-//> using dep io.opentelemetry:opentelemetry-sdk:1.49.0
-//> using dep io.opentelemetry:opentelemetry-sdk-trace:1.49.0
-//> using dep io.opentelemetry:opentelemetry-exporter-logging-otlp:1.49.0
+//> using dep io.opentelemetry:opentelemetry-sdk:1.50.0
+//> using dep io.opentelemetry:opentelemetry-sdk-trace:1.50.0
+//> using dep io.opentelemetry:opentelemetry-exporter-logging-otlp:1.50.0
 //> using dep io.opentelemetry.semconv:opentelemetry-semconv:1.32.0
 
 import io.opentelemetry.sdk.trace.SdkTracerProvider
