0fda34cb71d0cabd0be1b76b028358721a3d5288
Michael Nedokushev <grouzen.hexy@gmail.com>
2025-06-19 19:17:37 +0100

    Add Gauge metric instrument

--- opentelemetry/src/test/scala/zio/telemetry/opentelemetry/metrics/MeterTest.scala
+++ opentelemetry/src/test/scala/zio/telemetry/opentelemetry/metrics/MeterTest.scala
@@ -129,7 +129,7 @@ object MeterTest extends ZIOSpecDefault {
           for {
             reader          <- ZIO.service[InMemoryMetricReader]
             counter         <- meter.counter("test_counter")
-            attributes       = Attributes(Attribute.long("attr1", 3L))
+            attributes       = Attributes(Attribute.long("attr_counter", 3L))
             _               <- counter.add(12, attributes)
             _               <- counter.inc(attributes)
             metric           = reader.collectAllMetrics().asScala.toList.head
@@ -147,7 +147,7 @@ object MeterTest extends ZIOSpecDefault {
           for {
             reader          <- ZIO.service[InMemoryMetricReader]
             counter         <- meter.upDownCounter("test_up_down_counter")
-            attributes       = Attributes(Attribute.boolean("attr2", value = false))
+            attributes       = Attributes(Attribute.boolean("attr_up_down_counter", value = false))
             _               <- counter.add(5, attributes)
             _               <- counter.inc(attributes)
             _               <- counter.dec(attributes)
@@ -162,12 +162,31 @@ object MeterTest extends ZIOSpecDefault {
           )
         }
       },
+      test("gauge") {
+        ZIO.serviceWithZIO[Meter] { meter =>
+          for {
+            reader          <- ZIO.service[InMemoryMetricReader]
+            gauge           <- meter.gauge("test_gauge")
+            attributes       = Attributes(Attribute.boolean("attr_gauge", value = false))
+            _               <- gauge.set(10.0, attributes)
+            _               <- gauge.set(20.0, attributes)
+            _               <- gauge.set(-5.6, attributes)
+            metric           = reader.collectAllMetrics().asScala.toList.head
+            metricPoint      = metric.getDoubleGaugeData().getPoints().asScala.toList.head
+            metricValue      = metricPoint.getValue()
+            metricAttributes = metricPoint.getAttributes()
+          } yield assertTrue(
+            metricValue == -5.6,
+            metricAttributes == attributes
+          )
+        }
+      },
       test("histogram") {
         ZIO.serviceWithZIO[Meter] { meter =>
           for {
             reader          <- ZIO.service[InMemoryMetricReader]
             histogram       <- meter.histogram("test_histogram")
-            attributes       = Attributes(Attribute.double("attr3", 12.3))
+            attributes       = Attributes(Attribute.double("attr_historgram", 12.3))
             _               <- histogram.record(2.1, attributes)
             _               <- histogram.record(3.3, attributes)
             metric           = reader.collectAllMetrics().asScala.toList.head
@@ -277,10 +296,10 @@ object MeterTest extends ZIOSpecDefault {
             _               <- ZIO.logAnnotate("zio", "annotation") {
                                  counter.inc(Attributes(Attribute.string("zio", "annotation2")))
                                }
-            metric           = reader.collectAllMetrics().asScala.toList.head
+            metric           = reader.collectAllMetrics.asScala.toList.head
             metricPoint      = metric.getLongSumData.getPoints.asScala.toList.head
             metricValue      = metricPoint.getValue
-            metricAttributes = metricPoint.getAttributes()
+            metricAttributes = metricPoint.getAttributes
           } yield assertTrue(
             metricValue == 1L,
             metricAttributes == Attributes(Attribute.string("zio", "annotation2"))
@@ -289,29 +308,72 @@ object MeterTest extends ZIOSpecDefault {
       }
     ).provide(inMemoryMetricReaderLayer, meterLayer(logAnnotated = true), ctxStorageLayer)
 
+  // TODO: add test case for Metric.frequency
   private val zioMetricsSpec =
     suite("ZIO metrics integration")(
-      test("histogram boundaries should be passed to OTEL") {
+      test("counter") {
+        val counter = Metric.counter("test_counter").tagged("zio", "counter")
+
+        for {
+          reader          <- ZIO.service[InMemoryMetricReader]
+          _               <- counter.incrementBy(3L)
+          metric           = reader.collectAllMetrics.asScala.find(_.getName == "test_counter").get
+          metricPoint      = metric.getLongSumData.getPoints.asScala.toList.head
+          metricValue      = metricPoint.getValue
+          metricAttributes = metricPoint.getAttributes
+        } yield assertTrue(metricValue == 3, metricAttributes == Attributes(Attribute.string("zio", "counter")))
+      },
+      test("gauge") {
+        val gauge = Metric.gauge("test_gauge").tagged("zio", "gauge")
+
+        for {
+          reader          <- ZIO.service[InMemoryMetricReader]
+          _               <- gauge.set(10.2)
+          metric           = reader.collectAllMetrics.asScala.find(_.getName == "test_gauge").get
+          metricPoint      = metric.getDoubleGaugeData.getPoints.asScala.toList.head
+          metricValue      = metricPoint.getValue
+          metricAttributes = metricPoint.getAttributes
+        } yield assertTrue(metricValue == 10.2, metricAttributes == Attributes(Attribute.string("zio", "gauge")))
+      },
+      test("histogram") {
         val histogram = Metric.histogram("test_histogram", Boundaries.fromChunk(Chunk(1, 2, 3)))
 
         for {
-          reader     <- ZIO.service[InMemoryMetricReader]
-          _          <- histogram.update(2.0)
-          metric      = reader.collectAllMetrics().asScala.find(_.getName == "test_histogram").get
-          metricPoint = metric.getHistogramData().getPoints().asScala.toList.head
-          boundaries  = metricPoint.getBoundaries.asScala.map(_.toDouble).toSeq
-        } yield assertTrue(boundaries == Seq(1.0, 2.0, 3.0))
+          reader          <- ZIO.service[InMemoryMetricReader]
+          _               <- histogram.update(3.0)
+          _               <- histogram.update(1.5)
+          metric           = reader.collectAllMetrics.asScala.find(_.getName == "test_histogram").get
+          metricPoint      = metric.getHistogramData.getPoints.asScala.toList.head
+          metricMaxValue   = metricPoint.getMax
+          metricMinValue   = metricPoint.getMin
+          metricCountValue = metricPoint.getCount
+          metricBoundaries = metricPoint.getBoundaries.asScala.map(_.toDouble).toSeq
+        } yield assertTrue(
+          metricMaxValue == 3.0,
+          metricMinValue == 1.5,
+          metricCountValue == 2,
+          metricBoundaries == Seq(1.0, 2.0, 3.0)
+        )
       },
-      test("timer boundaries should be passed to OTEL") {
+      test("timer") {
         val timer = Metric.timer("test_timer", ChronoUnit.SECONDS, Chunk(1.0, 2.0, 3.0))
 
         for {
-          reader     <- ZIO.service[InMemoryMetricReader]
-          _          <- timer.update(Duration.fromSeconds(2))
-          metric      = reader.collectAllMetrics().asScala.find(_.getName == "test_timer").get
-          metricPoint = metric.getHistogramData().getPoints().asScala.toList.head
-          boundaries  = metricPoint.getBoundaries.asScala.map(_.toDouble).toSeq
-        } yield assertTrue(boundaries == Seq(1.0, 2.0, 3.0))
+          reader          <- ZIO.service[InMemoryMetricReader]
+          _               <- timer.update(Duration.fromSeconds(1))
+          _               <- timer.update(Duration.fromSeconds(2))
+          metric           = reader.collectAllMetrics.asScala.find(_.getName == "test_timer").get
+          metricPoint      = metric.getHistogramData.getPoints.asScala.toList.head
+          metricMaxValue   = metricPoint.getMax
+          metricMinValue   = metricPoint.getMin
+          metricCountValue = metricPoint.getCount
+          metricBoundaries = metricPoint.getBoundaries.asScala.map(_.toDouble).toSeq
+        } yield assertTrue(
+          metricMaxValue == 2,
+          metricMinValue == 1,
+          metricCountValue == 2,
+          metricBoundaries == Seq(1.0, 2.0, 3.0)
+        )
       }
     ).provide(
       inMemoryMeterProvider,
