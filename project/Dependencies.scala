00832697207c1a9ee278fbd97d8277d52934973d
Scala Steward <scala_steward@virtuslab.com>
2025-07-21 21:22:48 +0000

    Update zio, zio-stacktracer, zio-test, ... to 2.1.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val opentelemetry         = "1.52.0"
     val opencensus            = "0.31.1"
     val scalaCollectionCompat = "2.13.0"
-    val zio                   = "2.1.19"
+    val zio                   = "2.1.20"
     val zioLogging            = "2.5.1"
     val izumiReflect          = "3.0.5"
   }
