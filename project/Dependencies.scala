95f7f17c30a98970b0898857c26e7fa1ff273034
Michael Nedokushev <grouzen.hexy@gmail.com>
2026-04-23 14:43:47 +0200

    Bump opentelemetry-api-incubator to 1.61.0-alpha
    Aligns the incubator artifact version with the main opentelemetry-sdk
    1.61.0 version to fix a missinglink conflict: DeclarativeConfigProperties.get(String)
    was missing from the older 1.53.0-alpha jar.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val opentracing            = "0.33.0"
     val opentelemetry          = "1.61.0"
     val opentelemetryContrib   = "1.48.0-alpha"
-    val opentelemetryIncubator = "1.53.0-alpha"
+    val opentelemetryIncubator = "1.61.0-alpha"
     val opencensus             = "0.31.1"
     val scalaCollectionCompat  = "2.14.0"
     val zio                    = "2.1.25"
